057965c3fc76fdfaa98e0a70b9969096661c26c0
Jason A. Birdsong <birdsongj6@udayton.edu>
2024-10-21 10:50:37 -0400

    models file taken from candyland modified to users

---
+++ src/models/model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const UserScehma = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        maxLenngth: 25
+    },
+    password: {
+        type: String,
+        required: true,
+        maxLength: 25
+    },
+    cheese: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+    profilepicture: {
+        type: Number,
+        required: true,
+    },
+});
+
+UserSchema.statics.findByName = async function(query){
+    return await this.findOne({username: query,});
+};
+
+UserSchema.statics.getAll = async function(){
+    return await this.find({});
+};
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel
+
+
+
