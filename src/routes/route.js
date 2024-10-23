c1d2a4f426ae66125fa49747026791ca698ffc0e
Jason A. Birdsong <birdsongj6@udayton.edu>
2024-10-23 10:33:58 -0400

    base router functionality with outline

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,5 +7,18 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+//router.post call function to go to do signup
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+//router.post call function to do login
+
+
+//protected page here
+
 module.exports = router;
 
