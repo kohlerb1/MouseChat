3a10e8ec3a24180b8675729702785b9189b7df46
Jason A. Birdsong <birdsongj6@udayton.edu>
2024-11-13 10:20:19 -0500

    testing sockets lab in mousechat

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
 const app = express();
 const bodyParser = require('body-parser');
 const multer = require('multer');
@@ -6,8 +9,13 @@ const storage = multer.memoryStorage();
 //const upload = multer({ storage: storage });
 const upload = multer({ storage:storage });
 const session = require('express-session');
+const server = createServer(app);
+const io = new Server(server);
 
 const db = require('./config/db');
+
+app.use('/static', express.static('static'));
+app.use('/public', express.static('public'));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
@@ -16,6 +24,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 //app.use(upload.array());
 app.use(session( {secret: "Mellon"}));
+
+//************************************************ */
+app.get('/', (req, res) => {            
+    const basename = new URL('./index.html', import.meta.url).pathname;
+    const trimmedname = basename.replace('/C:', '');
+    res.sendFile(trimmedname);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+//*********************************************** */
+
 
 const router = require('./routes');
 app.use('/', router.Router);
