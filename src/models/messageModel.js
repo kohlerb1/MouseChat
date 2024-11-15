5d93c3030630220e6cac088bebd7e5d5fab87ef8
Ryan A. Eagan <eaganr1@udayton.edu>
2024-11-15 10:23:54 -0500

    Add recipient to message model

--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -6,6 +6,10 @@ const messageSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         required: true
     },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
     content: {
         type: String,
         required: true
