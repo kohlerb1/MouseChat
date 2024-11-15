7c7eead25ba1900ceac6f7aea6d65724abdd2796
johnsonl44 <johnsonl44@udayton.edu>
2024-11-15 09:23:28 -0500

    fixed conflict hopefully

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
 const app = express();
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const storage = multer.memoryStorage();
 //const upload = multer({ storage: storage });
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 const session = require('express-session');
 
+const server = http.createServer(app);
+const io = socketIo(server);
 const db = require('./config/db');
 
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
+
+//app.use('views', express.static('views'));
+app.use('/public', express.static('public'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-//app.use(upload.array());
-app.use(session( {secret: "Mellon"}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: "Mellon" }));
 app.use(express.static('views'));
+
+//************************************************ */
+module.exports = io;
+//************************************************ */
+
 const router = require('./routes');
 app.use('/', router.Router);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
